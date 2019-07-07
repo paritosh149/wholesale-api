3bf5e3336e8665f8506df5e332c632df24a4abe6
Paritosh Mishra <paritosh149@outlook.com>
2019-07-07 19:42:00 +1000

    Initial Starter project committed

---
+++ anz_wholesale_api/src/main/java/com/dummy/api/AnzWholesaleApiApplication.java
@@ -0,0 +1,13 @@
+package com.dummy.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnzWholesaleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnzWholesaleApiApplication.class, args);
+	}
+
+}
