a7439112d9e85cf0e27b3cc223f322a6f01e796e
Paritosh Mishra <paritosh149@outlook.com>
2019-07-07 23:09:45 +1000

    Repositories allowed GET only. PUT, POST and DELETE are not exported over REST

--- anz_wholesale_api/src/main/java/com/dummy/api/repository/AccountRepository.java
+++ anz_wholesale_api/src/main/java/com/dummy/api/repository/AccountRepository.java
@@ -1,9 +1,33 @@
 package com.dummy.api.repository;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.dummy.api.model.Account;
 
 public interface AccountRepository extends PagingAndSortingRepository<Account, Long> {
+	@Override
+	@RestResource(exported = false)
+	<S extends Account> S save(S entity);
+
+	@Override
+	@RestResource(exported = false)
+	<S extends Account >Iterable<S> saveAll(Iterable<S> entities);
+
+	@Override
+	@RestResource(exported = false)
+	void deleteById(Long id) ;
+	
+	@Override
+	@RestResource(exported = false)
+	void delete(Account entity);
+	
+	@Override
+	@RestResource(exported = false)
+	void deleteAll(Iterable<? extends Account> entities) ;
+
+	@Override
+	@RestResource(exported = false)
+	void deleteAll() ;
 
 }
--- anz_wholesale_api/src/main/java/com/dummy/api/repository/TransactionRepository.java
+++ anz_wholesale_api/src/main/java/com/dummy/api/repository/TransactionRepository.java
@@ -1,9 +1,33 @@
 package com.dummy.api.repository;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.dummy.api.model.Transaction;
 
 public interface TransactionRepository extends PagingAndSortingRepository<Transaction, Long> {
+	@Override
+	@RestResource(exported = false)
+	<S extends Transaction> S save(S entity);
+
+	@Override
+	@RestResource(exported = false)
+	<S extends Transaction> Iterable<S> saveAll(Iterable<S> entities);
+
+	@Override
+	@RestResource(exported = false)
+	void deleteById(Long id) ;
+
+	@Override
+	@RestResource(exported = false)
+	void delete(Transaction entity) ;
+
+	@Override
+	@RestResource(exported = false)
+	void deleteAll(Iterable<? extends Transaction> entities) ;
+
+	@Override
+	@RestResource(exported = false)
+	void deleteAll() ;
 
 }
