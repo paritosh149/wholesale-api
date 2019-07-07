9cd85d8301cef73c0c7cf64fa6a7b272b63b6a14
Paritosh Mishra <paritosh149@outlook.com>
2019-07-07 20:08:19 +1000

    Model added - Account

---
+++ anz_wholesale_api/src/main/java/com/dummy/api/model/Account.java
@@ -0,0 +1,30 @@
+package com.dummy.api.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Account {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	private String name;
+
+}
